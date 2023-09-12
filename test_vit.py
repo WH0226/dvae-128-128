09cf511fa1ae4ec23bbe53ba4dad55ee67960d7d
Wang0226 <1037516307@qq.com>
2023-09-12 11:45:32 +0800

    firstcommit

---
+++ test_vit.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+from timm.models.layers import StdConv2dSame, DropPath, to_2tuple, trunc_normal_
+
+
+class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding"""
+
+    def __init__(
+        self,
+        img_size=224,
+        patch_size=16,
+        in_chans=3,
+        embed_dim=768,
+        no_patch_embed_bias=False,
+    ):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+
+        self.proj = nn.Conv2d(
+            in_chans,
+            embed_dim,
+            kernel_size=patch_size,
+            stride=patch_size,
+            bias=False if no_patch_embed_bias else True,
+        )
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        # FIXME look at relaxing size constraints
+        x = self.proj(x)
+        return x
+
+mytest = PatchEmbed()
+img = torch.rand(64, 3, 256, 256)
+# img = img.double()
+# img = torch.from_numpy(img)
+print(img.shape)
+out = mytest(img)
+print(out.shape)
